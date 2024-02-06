9aeef66fae69a8a2ef127cd9232f4f05c29b504b
Fletcher Passow <fletcherpassow@icloud.com>
2024-02-06 13:54:52 -0800

    Set up initial preprocess pipeline

--- src/megaPLuG/pipelines/preprocess/nodes.py
+++ src/megaPLuG/pipelines/preprocess/nodes.py
@@ -4,7 +4,7 @@ generated using Kedro 0.19.1
 """
 import dask.dataframe as dd
 
-def preprocess_navistar(navistar, params):
+def format_navistar_columns(navistar, params):
     """Preprocess navistar data as a dask DataFrame object."""
     navistar = navistar.categorize(params["category_columns"])
 
@@ -12,3 +12,13 @@ def preprocess_navistar(navistar, params):
         navistar[col] = dd.to_datetime(navistar[col], utc=True)
 
     return navistar.compute()
+
+
+def build_h3_polygons(us_outline):
+    """Build H3 Scale 8 polygons for full continental U.S."""
+    return h3_polygons
+
+
+def clean_vius(vius):
+    """Remove unnecessary VIUS columns and calculate new ones, if necessary."""
+    return vius
--- src/megaPLuG/pipelines/preprocess/pipeline.py
+++ src/megaPLuG/pipelines/preprocess/pipeline.py
@@ -5,18 +5,44 @@ generated using Kedro 0.19.1
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import preprocess_navistar
+from .nodes import format_navistar_columns, build_h3_polygons, clean_vius
+from .process_role_consumption import calc_energy_consump_rate
+from .process_role_energy import label_charging_sessions
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     pipe = pipeline(
         [
             node(
-                func=preprocess_navistar,
+                func=format_navistar_columns,
                 inputs=["navistar", "params:navistar"],
                 outputs="trips",
-                name="preprocess_navistar",
-            )
+                name="format_navistar_columns",
+            ),
+            node(
+                func=build_h3_polygons,
+                inputs="us_outline",
+                outputs="h3_8_polygons",
+                name="build_h3_polygons",
+            ),
+            node(
+                func=clean_vius,
+                inputs="vius_raw",
+                outputs="vius_cleaned",
+                name="clean_vius",
+            ),
+            node(
+                func=label_charging_sessions,
+                inputs=["role_energy_readings", "params:charge_sessions"],
+                outputs="energy_sessions",
+                name="preprocess_energy_sessions",
+            ),
+            node(
+                func=calc_energy_consump_rate,
+                inputs=["energy_sessions", "role_gps", "params:consump_rate"],
+                outputs="energy_consumption",
+                name="preprocess_energy_consumption",
+            ),
         ],
     )
     return pipe
