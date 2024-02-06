422d30ff4e6eb7c7fb1b1e21fb0cfefd65e132a8
Fletcher Passow <fletcherpassow@icloud.com>
2024-02-06 12:45:38 -0800

    Add electrify_trips pipeline

---
+++ src/megaPLuG/pipelines/electrify_trips/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'electrify_trips'
+generated using Kedro 0.19.1
+"""
---
+++ src/megaPLuG/pipelines/electrify_trips/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'electrify_trips'
+generated using Kedro 0.19.1
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
