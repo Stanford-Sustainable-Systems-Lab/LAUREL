d5e89770a8935c824ffe708620ccc364231f0ed2
Fletcher Passow <fletcherpassow@icloud.com>
2024-02-06 12:50:46 -0800

    Add evaluate_impacts pipeline

---
+++ src/megaPLuG/pipelines/evaluate_impacts/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'evaluate_impacts'
+generated using Kedro 0.19.1
+"""
---
+++ src/megaPLuG/pipelines/evaluate_impacts/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'evaluate_impacts'
+generated using Kedro 0.19.1
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
