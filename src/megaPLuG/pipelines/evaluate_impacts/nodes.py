fffd71a1897c78e15129ad33c7387ffe509931d0
Fletcher Passow <fletcherpassow@icloud.com>
2024-07-17 12:02:30 -0700

    Flip coordinates to lon-lat in mapping

--- src/megaPLuG/pipelines/evaluate_impacts/nodes.py
+++ src/megaPLuG/pipelines/evaluate_impacts/nodes.py
@@ -92,7 +92,11 @@ def add_geometries(df: pd.DataFrame, params: dict) -> gpd.GeoDataFrame:
 
 
 def h3_to_poly(h: int) -> Polygon:
-    return Polygon(h3.cell_to_boundary(h))
+    bnd = h3.cell_to_boundary(h)
+    # h3 outputs geometries in lat-lon format, but the convention in WGS84 is lon-lat
+    bnd_flip = [(x, y) for y, x in bnd]
+    poly = Polygon(bnd_flip)
+    return poly
 
 
 def aggregate_regional_loads(
