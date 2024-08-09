bf1c25e864ce39817678c490ece16f868b06fd25
Fletcher Passow <fletcherpassow@icloud.com>
2024-08-09 10:53:06 -0700

    Create hex series to geoseries functions

--- src/megaPLuG/utils/h3.py
+++ src/megaPLuG/utils/h3.py
@@ -1,3 +1,4 @@
+import geopandas as gpd
 import h3.api.numpy_int as h3
 import pandas as pd
 from shapely import Polygon
@@ -13,3 +14,21 @@ def h3_to_poly(h: int) -> Polygon:
     bnd_flip = [(x, y) for y, x in bnd]
     poly = Polygon(bnd_flip)
     return poly
+
+
+def cells_to_points(s: pd.Series) -> gpd.GeoSeries:
+    """Creates a like-indexed GeoSeries of points from a series of h3 cells."""
+    tups = s.transform(h3.cell_to_latlng)
+    lats = tups.transform(lambda t: t[0]).values
+    lngs = tups.transform(lambda t: t[1]).values
+    pts = gpd.GeoSeries.from_xy(lngs, lats, crs="EPSG:4326", index=s.index)
+    return pts
+
+
+def cells_to_polygons(s: pd.Series) -> gpd.GeoSeries:
+    """Creates a like-indexed GeoSeries of polygons from a series of h3 cells."""
+    bnds = s.transform(h3.cell_to_boundary)
+    flip_bnds = bnds.transform(lambda bnd: [(x, y) for y, x in bnd])
+    polys = flip_bnds.transform(Polygon)
+    poly_ser = gpd.GeoSeries(polys, crs="EPSG:4326", index=s.index)
+    return poly_ser
