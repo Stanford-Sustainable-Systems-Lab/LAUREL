1300ce1328437aab0f2e396142ef75aaba6c8e15
Fletcher Passow <fletcherpassow@icloud.com>
2024-08-27 16:13:55 -0700

    Downgrade to h3 v3.7.7 to enable timezonefinder

--- src/megaPLuG/utils/h3.py
+++ src/megaPLuG/utils/h3.py
@@ -5,11 +5,11 @@ from shapely import Polygon
 
 
 def str_to_h3(s: pd.Series) -> pd.Series:
-    return s.transform(h3.str_to_int)
+    return s.transform(h3.string_to_h3)
 
 
 def h3_to_poly(h: int) -> Polygon:
-    bnd = h3.cell_to_boundary(h)
+    bnd = h3.h3_to_geo_boundary(h)
     # h3 outputs geometries in lat-lon format, but the convention in WGS84 is lon-lat
     bnd_flip = [(x, y) for y, x in bnd]
     poly = Polygon(bnd_flip)
@@ -18,7 +18,7 @@ def h3_to_poly(h: int) -> Polygon:
 
 def cells_to_points(s: pd.Series) -> gpd.GeoSeries:
     """Creates a like-indexed GeoSeries of points from a series of h3 cells."""
-    tups = s.transform(h3.cell_to_latlng)
+    tups = s.transform(h3.h3_to_geo)
     lats = tups.transform(lambda t: t[0]).values
     lngs = tups.transform(lambda t: t[1]).values
     pts = gpd.GeoSeries.from_xy(lngs, lats, crs="EPSG:4326", index=s.index)
@@ -27,7 +27,7 @@ def cells_to_points(s: pd.Series) -> gpd.GeoSeries:
 
 def cells_to_polygons(s: pd.Series) -> gpd.GeoSeries:
     """Creates a like-indexed GeoSeries of polygons from a series of h3 cells."""
-    bnds = s.transform(h3.cell_to_boundary)
+    bnds = s.transform(h3.h3_to_geo_boundary)
     flip_bnds = bnds.transform(lambda bnd: [(x, y) for y, x in bnd])
     polys = flip_bnds.transform(Polygon)
     poly_ser = gpd.GeoSeries(polys, crs="EPSG:4326", index=s.index)
