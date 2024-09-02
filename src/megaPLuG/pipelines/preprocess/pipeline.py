06662ed530a4e3d82596473fadfa11e0fcfeb9ea
Fletcher Passow <fletcherpassow@icloud.com>
2024-09-02 11:03:49 -0700

    Bring back preprocess pipeline

---
+++ src/megaPLuG/pipelines/preprocess/pipeline.py
@@ -0,0 +1,45 @@
+"""
+This is a boilerplate pipeline 'preprocess'
+generated using Kedro 0.19.1
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+from .nodes import (
+    calc_derived_trip_cols,
+    create_dwells,
+    format_trips_columns,
+    strip_vehicle_attrs,
+)
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    pipe = pipeline(
+        [
+            node(
+                func=format_trips_columns,
+                inputs=["navistar", "params:format_columns"],
+                outputs="trips_formatted",
+                name="format_trips_columns",
+            ),
+            node(
+                func=strip_vehicle_attrs,
+                inputs=["trips_formatted", "params:strip_vehicle_attrs"],
+                outputs=["trips_stripped", "vehicles_raw"],
+                name="strip_vehicle_attrs",
+            ),
+            node(
+                func=calc_derived_trip_cols,
+                inputs=["trips_stripped", "params:trip_derived_cols"],
+                outputs="trips_derived",
+                name="calc_derived_trip_cols",
+            ),
+            node(
+                func=create_dwells,
+                inputs=["trips_derived", "params:create_dwells"],
+                outputs="dwells",
+                name="create_dwells",
+            ),
+        ],
+    )
+    return pipe
