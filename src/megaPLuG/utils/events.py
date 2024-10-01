929915323e47624c33f9161af9ad8d9fcd7f1f37
Fletcher Passow <fletcherpassow@icloud.com>
2024-10-01 11:52:43 -0700

    Convert get_events to numba loop

--- src/megaPLuG/utils/events.py
+++ src/megaPLuG/utils/events.py
@@ -1,21 +1,89 @@
 """
-Utility functions
+Utility functions for events
 by Fletcher Passow
-February 2023
+October 2024
 """
 
-from collections.abc import Callable
+import logging
 
 import numpy as np
 import pandas as pd
+from numba import njit
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
 
 
 def get_events(
-    obs_ser: pd.Series,
-    time_ser: pd.Series,
-    grp_ser: pd.Series,
-    obs_in_event: Callable = (lambda x: x > 0),
-    max_time_elapsed: int = 6 * 60,
+    df: pd.DataFrame,
+    include_col: str,
+    dur_col: str,
+    grp_col: str,
+    out_col: str = "event_id",
+    max_time_elapsed: pd.Timedelta = pd.Timedelta(0, "s"),
+) -> pd.DataFrame:
+    """Get an event id column which gives the indices for events based on periodic
+    observations.
+
+    Events are defined as semi-contiguous stretches of a value matching the
+    criterion given by obs_in_event. The breaks in an event can only be as long as
+    max_time_elapsed.
+
+    This function depends on sorting by time within each group, and that groups are
+    each one block of rows.
+
+    Args:
+      df: DataFrame to add the event_id column to
+      include_col: the name of a boolean series which is True for every row (observation) that
+        should be in an event and False for every row (observation) that should NOT be
+        in an event.
+      dur_col: the name of a series of durations (as pandas TimeDeltas) of the events given in include_col
+      grp_col: the name of a series of markers for groups of observations, usually this would be a series
+        of integer ids, but if None, then all observations are assumed to
+        come from the same group.
+      max_time_elapsed: pandas TimeDelta giving the maximum time between events for them
+              to be combined into a single event.
+
+    Returns: a dataframe with a new column which gives an index of which event a particular
+    observation is a part of (or zero if the observation is part of no event).
+    Zero is used instead of a null value to mark the non-events because it allows
+    the new column to be cast to an integer type, which facilitates merging.
+    """
+    df["secs_elapsed"] = df[dur_col].dt.total_seconds()
+    df[out_col] = 0
+    tqdm.pandas()
+    df = df.groupby(grp_col, group_keys=False, sort=False).progress_apply(
+        func=get_events_wrapper,
+        include_col=include_col,
+        secs_elapsed_col="secs_elapsed",
+        out_col=out_col,
+        max_time_elapsed=max_time_elapsed,
+    )
+    df = df.drop(columns=["secs_elapsed"])
+    return df
+
+
+def get_events_wrapper(
+    grp: pd.DataFrame,
+    include_col: str,
+    secs_elapsed_col: str,
+    out_col: str,
+    max_time_elapsed: pd.Timedelta,
+) -> pd.DataFrame:
+    """Take the group and convert it into numba-compatible types."""
+    grp[out_col] = get_events_core(
+        include=grp[include_col].values,
+        secs_elapsed=grp[secs_elapsed_col].values,
+        max_secs_elapsed=max_time_elapsed.total_seconds(),
+    )
+    return grp
+
+
+@njit
+def get_events_core(
+    include: np.ndarray[bool],
+    secs_elapsed: np.ndarray[float],
+    max_secs_elapsed: float = 0.0,
 ) -> np.ndarray:
     """Get a ndarray which gives the indices for events based on periodic
     observations.
@@ -24,109 +92,34 @@ def get_events(
     criterion given by obs_in_event. The breaks in an event can only be as long as
     max_time_elapsed.
 
-    Args:
-      obs_ser: periodic observations to base groups off of
-      time_ser: time (as pandas timestamps) when observations in obs_ser were taken
-      grp_ser: marker for groups of observations, usually this would be a series
-        of integer geyser ids, but if None, then all observations are assumed to
-        come from the same group.
-      obs_in_event: function taking a series and returning a boolean series which is True for
-        every row (observation) that should be in an event and False for every row (observation)
-        that should NOT be in an event.
-      max_time_elapsed: integer giving the maximum time in seconds between events for them
-              to be combined into a single event.
-
-    Returns: a numpy ndarray which gives an index of which event a particular
-    observation is a part of (or zero if the observation is part of no event).
-    Zero is used instead of a null value to mark the non-events because it allows
-    the numpy ndarray to be cast to an integer type, which facilitates merging.
+    Assues interval-beginning time stamps, so events are marked at their beginning,
+    with the duration assumed to follow.
     """
-    if grp_ser is None:
-        grp_ser = pd.Series(0, index=obs_ser.index)
-
-    event_df = pd.DataFrame(
-        {"grp": grp_ser.values, "time": time_ser.values, "obs": obs_ser.values}
-    )
-    event_df = event_df.set_index(["grp", "time"])
-    if not event_df.index.is_monotonic_increasing:
-        raise Exception(
-            "The given combination of groups and times is not monotonic increasing."
-        )
-
-    def get_events_geyser(meas_ser, obs_in_event, max_time_elapsed):
-        """Get a series which gives the indices for events for a single geyser."""
-
-        if meas_ser.index.get_level_values("grp").nunique() > 1:
-            raise Exception("meas_ser contains more than one grouping.")
-
-        obs_out_event = lambda x: np.logical_not(obs_in_event(x))
-
-        not_in_event_ser = meas_ser.transform(obs_out_event)
-        event_id_ser = not_in_event_ser.cumsum() + 1
-        event_id_ser.loc[not_in_event_ser] = 0
-        event_df = pd.DataFrame(event_id_ser.rename("event_id")).reset_index()
-
-        if max_time_elapsed == 0:
-            return event_df["event_id"].values
-        else:
-            # Group events that are near to one another in time
-            increment_event_id_group = lambda x: (x > max_time_elapsed) | np.isnan(x)
-            group_df = (
-                event_df.query("event_id > 0")
-                .groupby("event_id", as_index=False)
-                .agg(
-                    event_start_time=pd.NamedAgg("time", "first"),
-                    event_end_time=pd.NamedAgg("time", "last"),
-                )
-                .assign(
-                    seconds_elapsed=lambda x: (
-                        x["event_start_time"] - x["event_end_time"].shift(1)
-                    ).dt.total_seconds()
-                )
-                .assign(
-                    event_id_group=lambda x: (
-                        increment_event_id_group(x["seconds_elapsed"])
-                    ).cumsum()
-                )
-            )
-            group_df = group_df.drop(
-                columns=np.setdiff1d(group_df.columns, ["event_id", "event_id_group"])
-            )
-
-            # Apply event groups to measurements
-            event_df = event_df.merge(group_df, how="left", on="event_id")
-            event_df["event_id_group_bfill"] = event_df["event_id_group"].bfill()
-            event_df["event_id_group_ffill"] = event_df["event_id_group"].ffill()
-            event_df["fill_match"] = (
-                event_df["event_id_group_bfill"] == event_df["event_id_group_ffill"]
-            )
-            event_df.loc[event_df["fill_match"], "event_id_group"] = event_df.loc[
-                event_df["fill_match"], "event_id_group_bfill"
-            ]
-            event_df["event_id_group"] = (
-                event_df["event_id_group"].replace(np.nan, 0).astype(int)
-            )
-            return event_df["event_id_group"].values
-
-    event_df["event_id_group"] = (
-        event_df["obs"]
-        .groupby("grp")
-        .transform(
-            get_events_geyser,
-            obs_in_event=obs_in_event,
-            max_time_elapsed=max_time_elapsed,
-        )
-    )
-
-    # Create unique event ids across all geysers
-    event_df = event_df.reset_index()
-    uniq_df = event_df.loc[
-        event_df["event_id_group"] > 0, ["grp", "event_id_group"]
-    ].drop_duplicates()
-    uniq_df["event_id_uniq"] = np.arange(uniq_df.shape[0]) + 1
-    event_df = event_df.merge(uniq_df, how="left", on=["grp", "event_id_group"])
-
-    # Clean up for return
-    event_df["event_id_uniq"] = event_df["event_id_uniq"].replace(np.nan, 0).astype(int)
-
-    return event_df["event_id_uniq"].values
+    nsteps = include.shape[0]
+    cur_event = 0
+    prev_incl = False
+    secs_since_event_end = max_secs_elapsed + 1
+    non_event_start_idx = 0
+    event_ids = np.empty(nsteps, dtype=np.int64)
+    for i in range(nsteps):
+        incl = include[i]
+        if incl:  # If this index is known to be in an event
+            if not prev_incl:  # Coming into an event from a non-event
+                if (
+                    secs_since_event_end < max_secs_elapsed
+                ):  # If not enough time has elapsed to declare a new event
+                    event_ids[non_event_start_idx:i] = cur_event
+                else:  # If enough time has elapsed to create a new event
+                    event_ids[non_event_start_idx:i] = 0
+                    cur_event += 1
+            event_ids[i] = cur_event
+        elif prev_incl:  # Just coming out of event
+            non_event_start_idx = i
+            secs_since_event_end = 0
+        secs_since_event_end += secs_elapsed[
+            i
+        ]  # Assuming interval-beginning timestamps
+        prev_incl = incl
+    if not incl:  # If the final observation is not in a group
+        event_ids[non_event_start_idx:nsteps] = 0  # Fill all tail event ids with zero
+    return event_ids
