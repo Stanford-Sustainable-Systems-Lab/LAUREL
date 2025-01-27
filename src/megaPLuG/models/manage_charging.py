aadd10524ccc2f3e743b7056b70bf7db8fb90271
Fletcher Passow <fletcherpassow@icloud.com>
2025-01-27 11:04:19 -0800

    Remove sorting from IndependentDwellChargingManager.get_events()

--- src/megaPLuG/models/manage_charging.py
+++ src/megaPLuG/models/manage_charging.py
@@ -143,9 +143,6 @@ class IndependentDwellChargingManager(AbstractChargingManager):
         # self.dw.data = self.dw.data.dropna(subset=self.dw.seq_names)
         self.dw.seq_names = self.seq_names
         events = self.dw.to_events(id_cols=[self.dw.hex, self.dw.veh])
-        events = events.set_index([self.dw.hex, self.suffixes["time"]])
-        events = events.sort_index()
-        events = events.reset_index(self.suffixes["time"], drop=False)
         return events
 
     def check_for_suffixes(self) -> list[str]:
