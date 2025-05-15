e69557efb1f301c4253bdb8f68e9fca5ab34144a
Fletcher Passow <fletcherpassow@icloud.com>
2025-05-15 15:35:15 -0700

    Add container debugging node to compute_routes pipeline

--- src/megaPLuG/pipelines/compute_routes/pipeline.py
+++ src/megaPLuG/pipelines/compute_routes/pipeline.py
@@ -3,9 +3,20 @@ This is a boilerplate pipeline 'compute_routes'
 generated using Kedro 0.19.3
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, node, pipeline
+
+from .nodes import manage_container
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    pipe = pipeline([])
+    pipe = pipeline(
+        [
+            node(
+                func=manage_container,
+                inputs="params:manage_container",
+                outputs=None,
+                name="manage_container",
+            ),
+        ],
+    )
     return pipe
