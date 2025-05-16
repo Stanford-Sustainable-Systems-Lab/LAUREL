ded184cd3d1d7a24ba9ea5632992802655c9888a
Fletcher Passow <passow@stanford.edu>
2025-05-16 15:36:58 -0700

    Build a toy compute_routes pipeline to confirm functionality

--- src/megaPLuG/pipelines/compute_routes/nodes.py
+++ src/megaPLuG/pipelines/compute_routes/nodes.py
@@ -4,12 +4,36 @@ generated using Kedro 0.19.3
 """
 
 from megaPLuG.models.routing.router import GraphhopperContainerRouter
+from routingpy import Graphhopper
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-def manage_container(server_params: dict) -> None:
-    """Learn to manage containers."""
+def import_graph(server_params: dict) -> None:
+    """Import a Graphhopper graph."""
     server = GraphhopperContainerRouter(
-        image=server_params["image"], graph_dir=server_params["graph_dir"]
+        image=server_params["image"],
+        graph_dir=server_params["graph_dir"],
+        config_path=server_params["config_path"],
+        mem_max_gb=server_params["resources"]["mem_max_gb"],
+        mem_start_gb=server_params["resources"]["mem_start_gb"],
     )
-    server.import_graph(url=server_params["graph_url"])
-    print("hello world")
+    server.import_graph(input_file=server_params["map_path"])
+    logger.info("Import completed")
+
+
+def get_routes(route_params: dict, server_params: dict) -> None:
+    """Get routes using Graphhopper."""
+    coords = [(1.515137, 42.544508), (1.533161,42.555446)] # A short route in Andorra
+    with GraphhopperContainerRouter(
+        image=server_params["image"],
+        graph_dir=server_params["graph_dir"],
+        config_path=server_params["config_path"],
+        mem_max_gb=server_params["resources"]["mem_max_gb"],
+        mem_start_gb=server_params["resources"]["mem_start_gb"],
+    ) as server:
+        router = Graphhopper(base_url=server.base_url)
+        route = router.directions(locations=coords, profile=route_params["profile"])
+        logger.info(f"Route distance: {route.distance} meters")
--- src/megaPLuG/pipelines/compute_routes/pipeline.py
+++ src/megaPLuG/pipelines/compute_routes/pipeline.py
@@ -5,17 +5,23 @@ generated using Kedro 0.19.3
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import manage_container
+from .nodes import import_graph, get_routes
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     pipe = pipeline(
         [
             node(
-                func=manage_container,
-                inputs="params:manage_container",
+                func=import_graph,
+                inputs="params:graphhopper",
                 outputs=None,
-                name="manage_container",
+                name="import_graph",
+            ),
+            node(
+                func=get_routes,
+                inputs=["params:get_routes", "params:graphhopper"],
+                outputs=None,
+                name="get_routes",
             ),
         ],
     )
