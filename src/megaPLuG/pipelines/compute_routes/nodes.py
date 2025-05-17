717b3f1c505b81493a98d38539ef3965d12fc9b8
Fletcher Passow <passow@stanford.edu>
2025-05-16 18:10:00 -0700

    Split resource config into import and server sections

--- src/megaPLuG/pipelines/compute_routes/nodes.py
+++ src/megaPLuG/pipelines/compute_routes/nodes.py
@@ -13,12 +13,13 @@ logger = logging.getLogger(__name__)
 
 def import_graph(server_params: dict) -> None:
     """Import a Graphhopper graph."""
+    resource = server_params["resources"]["import"]
     server = GraphhopperContainerRouter(
         image=server_params["image"],
         graph_dir=server_params["graph_dir"],
         config_path=server_params["config_path"],
-        mem_max_gb=server_params["resources"]["mem_max_gb"],
-        mem_start_gb=server_params["resources"]["mem_start_gb"],
+        mem_max_gb=resource["mem_max_gb"],
+        mem_start_gb=resource["mem_start_gb"],
     )
     server.import_graph(input_file=server_params["map_path"])
     logger.info("Import completed")
@@ -26,13 +27,13 @@ def import_graph(server_params: dict) -> None:
 
 def get_routes(route_params: dict, server_params: dict) -> None:
     """Get routes using Graphhopper."""
-    coords = [(1.515137, 42.544508), (1.533161,42.555446)] # A short route in Andorra
+    resource = server_params["resources"]["server"]
     with GraphhopperContainerRouter(
         image=server_params["image"],
         graph_dir=server_params["graph_dir"],
         config_path=server_params["config_path"],
-        mem_max_gb=server_params["resources"]["mem_max_gb"],
-        mem_start_gb=server_params["resources"]["mem_start_gb"],
+        mem_max_gb=resource["mem_max_gb"],
+        mem_start_gb=resource["mem_start_gb"],
     ) as server:
         router = Graphhopper(base_url=server.base_url)
         route = router.directions(locations=coords, profile=route_params["profile"])
