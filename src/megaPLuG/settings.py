0c718878c488b12d62d059cca84b697ab7d0b230
Fletcher Passow <fletcherpassow@icloud.com>
2025-07-29 16:06:21 -0400

    Switch to using KedroDataCatalog instead of DataCatalog

--- src/megaPLuG/settings.py
+++ src/megaPLuG/settings.py
@@ -34,5 +34,6 @@ CONFIG_LOADER_ARGS = {
 # CONTEXT_CLASS = KedroContext
 
 # Class that manages the Data Catalog.
-# from kedro.io import DataCatalog
-# DATA_CATALOG_CLASS = DataCatalog
+from kedro.io import KedroDataCatalog
+
+DATA_CATALOG_CLASS = KedroDataCatalog
