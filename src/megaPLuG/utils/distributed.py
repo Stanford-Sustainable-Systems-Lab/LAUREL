cb14fdcd063918a724c2b477c7a32832f6f8ee61
Fletcher Passow <fletcherpassow@icloud.com>
2025-09-01 17:59:12 -0700

    Refactor Dask node management to ensure proper initialization and closure of LocalCluster and Client

--- src/megaPLuG/utils/distributed.py
+++ src/megaPLuG/utils/distributed.py
@@ -3,9 +3,12 @@ from dask.distributed import Client, LocalCluster
 
 def start_dask_node(params: dict) -> tuple[LocalCluster, Client]:
     """Start a Dask LocalCluster and client."""
-    cluster = LocalCluster(**params["cluster"])
-    client = Client(cluster)
-    return cluster, client
+    if ("use_dask" in params and params["use_dask"]) or ("use_dask" not in params):
+        cluster = LocalCluster(**params["cluster"])
+        client = Client(cluster)
+        return cluster, client
+    else:
+        return "None", "None"
 
 
 def stop_dask_node(cluster: LocalCluster, client: Client, result: object) -> None:
@@ -14,5 +17,8 @@ def stop_dask_node(cluster: LocalCluster, client: Client, result: object) -> Non
     result is used to ensure that this node runs last, after all desired results have
     been computed. Pass the final dataset which requires Dask to this node.
     """
-    cluster.close()
-    client.close()
+    if client != "None" and isinstance(client, Client):
+        client.close()
+
+    if cluster != "None" and isinstance(cluster, LocalCluster):
+        cluster.close()
