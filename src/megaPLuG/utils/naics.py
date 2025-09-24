7edc822fc050638a933cfdb3ccd2c00cfb5a6535
Fletcher Passow <fletcherpassow@icloud.com>
2025-09-23 17:49:29 -0700

    Add get_naics_leaf_class function for NAICS code classification

---
+++ src/megaPLuG/utils/naics.py
@@ -0,0 +1,25 @@
+import numpy as np
+from numba import jit
+
+
+@jit
+def get_naics_leaf_class(
+    codes: np.ndarray[int], leaves: np.ndarray[int], src_digits: int = 8
+) -> np.ndarray[int]:
+    """Get the NAICS 'leaf' for each NAICS code."""
+    out = np.zeros_like(codes)
+    used_leaves = np.zeros_like(leaves)
+    while np.any(used_leaves == 0) and np.any(codes > 0):
+        for i in range(len(leaves)):  # Check all leaves
+            leaf = leaves[i]
+            if not used_leaves[i] == 1:
+                matches = (codes == leaf) & (out == 0)
+                if np.any(matches):  # If there is a match
+                    out = np.where(matches, leaf, out)  # Replace out val
+                    used_leaves[i] = 1
+        codes = np.floor_divide(codes, 10)
+    # n_digits = np.where(out == 0, 1, np.floor(np.log10(out)) + 1)
+    # out = out * np.power(10, src_digits - n_digits)
+    if np.any(out == 0):
+        raise ValueError("At least some NAICS codes are assigned to no leaf code.")
+    return out
