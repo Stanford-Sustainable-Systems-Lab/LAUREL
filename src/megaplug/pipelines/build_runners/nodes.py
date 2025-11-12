8c96e7e68ecf0108e4568eb2a0b0bdbf27cc2213
Fletcher Passow <fletcherpassow@icloud.com>
2025-11-12 11:32:11 -0800

    Add ranger scenario

--- src/megaplug/pipelines/build_runners/nodes.py
+++ src/megaplug/pipelines/build_runners/nodes.py
@@ -15,6 +15,7 @@ from .batt_pow import BatteryPowerScenarioBuilder  # noqa: F401
 from .batt_dual_pow import BatteryDualPowerScenarioBuilder  # noqa: F401
 from .ca_eight import CalifClass8ScenarioBuilder  # noqa: F401
 from .ca_eight_adopt import CalifClass8AdoptionScenarioBuilder  # noqa: F401
+from .ranger import RangeScenarioBuilder  # noqa: F401
 from .scale_test import ScaleTestScenarioBuilder  # noqa: F401
 from .sense import SenseScenarioBuilder  # noqa: F401
 
