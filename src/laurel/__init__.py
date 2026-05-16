ccd8b1b0d10e6f905986ca5ebd99f096c94b8e2a
Fletcher Passow <20097255+fletp@users.noreply.github.com>
2026-05-15 17:08:24 -0700

    Add a warnings filter to suppress the FNV hashing warning from Numba

--- src/laurel/__init__.py
+++ src/laurel/__init__.py
@@ -1,3 +1,11 @@
 """EPA Phase III"""
 
 __version__ = "0.1"
+
+import warnings
+
+warnings.filterwarnings(
+    "ignore",
+    message="FNV hashing is not implemented in Numba",
+    category=UserWarning,
+)
