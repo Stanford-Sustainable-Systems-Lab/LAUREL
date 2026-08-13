8f6f3b3b9d8658136450023db63ed04dba10cd0d
Fletcher Passow <passow@stanford.edu>
2026-08-13 14:06:52 -0700

    Move validate scenario into updated code
    Both the generated catalog.yml, and the container-run pipelines

--- scripts/scenarios/validate.sh
+++ scripts/scenarios/validate.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #SBATCH --partition=serc
 #SBATCH --ntasks=2
-#SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=64GB
-#SBATCH --time=0-01:15
+#SBATCH --mem-per-cpu=16GB
+#SBATCH --time=0-01:00
 #SBATCH --array=0-0
 #SBATCH --job-name=validate
 #SBATCH --output=logs/slurm/%x_%A_%a.log
 
 cd conf && conf_dir=$(find scenarios/validate -type d -name "task_$SLURM_ARRAY_TASK_ID") && cd ..
-uv run kedro run --pipeline=electrify_trips --env=$conf_dir --params=data_dir=$SCRATCH/laurel/data
-uv run kedro run --pipeline=evaluate_impacts --env=$conf_dir --params=data_dir=$SCRATCH/laurel/data
+KEDRO_CONTAINER_DATA_DIR=$SCRATCH/laurel/data ./scripts/setup/run-in-container.sh kedro run --pipeline=electrify_trips --env=$conf_dir
+KEDRO_CONTAINER_DATA_DIR=$SCRATCH/laurel/data ./scripts/setup/run-in-container.sh kedro run --pipeline=evaluate_impacts --env=$conf_dir
